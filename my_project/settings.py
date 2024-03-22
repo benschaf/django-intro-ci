0912378711f4ad90785d6e7ec5df404dc494a572
Benjamin Schäfer <benibschafer@gmail.com>
2024-03-22 08:34:44 +0000

    Enable debug mode and add whitenoise to requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&@09r86n9-!vk0np$54ce!5_+b+$+3^kyfjljqb7wz0m@ju)b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
